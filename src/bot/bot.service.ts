d2e13b3e7d34e5eac858257a047e482c266f2cf3
mongopusher <urs.gramsall@protonmail.com>
2022-11-13 18:41:38 +0100

    do not use string.at

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -37,10 +37,7 @@ export class BotService {
     private async handleMessage(msg: Message): Promise<Message> {
         const userInput = msg.text.toString();
 
-        console.log('handle message: ', msg.text);
-        console.log('handle message as string: ', userInput);
-
-        if (userInput.at(0) === '/') {
+        if (userInput.slice(0, 1) === '/') {
             return await this.handleCommand(msg, userInput.slice(1, 0))
         }
 
