29abe6ac483a8f89bbca30bd4e708e2bb99b74db
mongopusher <urs.gramsall@protonmail.com>
2022-12-16 02:40:29 +0100

    try elo calc

--- src/elo/elo.constant.ts
+++ src/elo/elo.constant.ts
@@ -1,3 +1,5 @@
 export const ELO = {
     DEFAULT: 1000,
+    SCALE_FACTOR: 0.03,
+    BASE_GAIN: 20,
 }
--- src/elo/elo.service.ts
+++ src/elo/elo.service.ts
@@ -6,26 +6,65 @@ import {EloEntity} from "@webserver/elo/elo.entity";
 import {CreateEloDto} from "./dto/create-elo.dto";
 import {CupEntity} from "@webserver/cup/cup.entity";
 import {UserEntity} from "@webserver/user/user.entity";
+import {ELO} from "./elo.constant";
 
 @Injectable()
 export class EloService {
+
     public constructor(@InjectRepository(EloEntity) private readonly eloRepository: Repository<EloEntity>) {
 
     }
 
-    public async updateElos(cup: CupEntity, winners: Array<UserEntity>, losers: Array<UserEntity>): Promise<EloEntity> {
-        // make magic elo berechnung!
+    public async updateElos(cup: CupEntity, winners: Array<UserEntity>, losers: Array<UserEntity>): Promise<void> {
+        const eloEntities: Array<EloEntity & { newElo?: number }> = [];
 
-        console.log({
-            cup: cup.name,
-            winners: winners.map((user) => user.username).join(', '),
-            losers: losers.map((user) => user.username).join(', '),
-        });
+        let totalWinnerElo = 0;
+        let totalLoserElo = 0;
 
-        // const elo = new EloEntity();
+        for (const winner of winners) {
+            const eloEntity = await this.getEloByUserAndCup(winner, cup);
+            eloEntities.push(eloEntity);
+            totalWinnerElo += eloEntity.elo;
+        }
 
-        // return this.eloRepository.save(elo);
-        return '' as any;
+        for (const loser of losers) {
+            const eloEntity = await this.getEloByUserAndCup(loser, cup);
+            eloEntities.push(eloEntity);
+            totalLoserElo += eloEntity.elo;
+        }
+
+        const averageWinnerElo = totalWinnerElo / winners.length;
+        const averageLoserElo = totalLoserElo / losers.length;
+
+        let totalEloDiff = 0;
+
+        for (const eloEntity of eloEntities) {
+            let eloDiff = 0;
+            if (winners.findIndex((winner) => winner.id === eloEntity.user.id) !== -1) {
+                eloDiff = this.getNewElo(eloEntity.elo, averageLoserElo, true);
+            } else {
+                eloDiff = this.getNewElo(eloEntity.elo, averageWinnerElo, false);
+            }
+            eloEntity.newElo = eloEntity.elo + eloDiff;
+            totalEloDiff += eloDiff;
+        }
+
+        if (totalEloDiff !== 0) {
+            throw new Error('DIE HUAN RECHNUNG PASST NED');
+        }
+
+        for (const eloEntity of eloEntities) {
+            eloEntity.elo = eloEntity.newElo;
+            delete eloEntity.newElo;
+            delete eloEntity.updated_at;
+
+            await this.eloRepository.update({ id: eloEntity.id }, eloEntity);
+        }
+    }
+
+    private getNewElo(ownElo: number, enemyElo: number, isWinner: boolean): number {
+        const scaling = (ownElo - enemyElo) * ELO.SCALE_FACTOR;
+        return isWinner === true ? scaling + ELO.BASE_GAIN : scaling - ELO.BASE_GAIN;
     }
 
     public async createDefaultElo(createEloDto: CreateEloDto): Promise<EloEntity> {
@@ -48,9 +87,13 @@ export class EloService {
             .getMany();
     }
 
-    // public async getEloByUserAndCup(user: UserEntity, cup: CupEntity): Promise<EloEntity> {
-    //     return this.eloRepository.findOneBy({ user: user.id, cup: cup.id })
-    // }
+    public async getEloByUserAndCup(user: UserEntity, cup: CupEntity): Promise<EloEntity> {
+        return this.eloRepository.createQueryBuilder('elo')
+            .where('elo.userId = :userId', { userId: user.id })
+            .andWhere('elo.cupId = :cupId', { cupId: cup.id })
+            .getOne();
+    }
+
     //
     // public async getElosByUser(user: UserEntity): Promise<Array<EloEntity>> {
     //     return this.eloRepository.findBy({ user })
